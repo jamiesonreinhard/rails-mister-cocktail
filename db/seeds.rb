9d602307f8e5fb3ac297d3e81b736b4ee4369eb8
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2020-08-13 17:14:47 -0500

    site is functioning

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+array = ingredients["drinks"]
+
+array.each do |ingredient|
+  new_ingredient = Ingredient.new(name: ingredient["strIngredient1"])
+  new_ingredient.save
+end
